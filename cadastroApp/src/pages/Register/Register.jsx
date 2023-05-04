06995a7c9cd4496f4b9b7296bab516682f2b4c73
Vinicius Santos <viniciusantos0898@gmail.com>
2023-05-04 12:36:18 -0300

    Finished the register page style

--- cadastroApp/src/pages/Register/Register.jsx
+++ cadastroApp/src/pages/Register/Register.jsx
@@ -2,16 +2,49 @@ import { useState, useEffect } from 'react'
 import './Register.css'
 
 export function Register(){
-
+    const [user, setUser] = useState({username: '', password: '', name: '', city: ''})
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [name, setName] = useState('')
+    const [city, setCity] = useState('')
+    
+    function createUser(){
+        console.log(`${username}, ${password}, ${name}, ${city}`)
+    }
+    
     return (
         <div className="registerPage">
-            <h1> Cadastro </h1>
+            <header> <h1> Cadastro </h1> </header>
             
-            <div className='registerForm'>
-            <form> 
-                
-            </form>
-            </div>
+            <main> 
+                <div className='registerForm'>
+                    <form> 
+                        <input 
+                            type='text' 
+                            placeholder='Digite Seu Username'
+                            onChange={e => setUsername(e.target.value)}
+                        /> 
+                        <input 
+                            type='password' 
+                            placeholder='Digite sua senha'
+                            onChange={e => setPassword(e.target.value)}
+                        /> 
+                        <input 
+                            type='text' 
+                            placeholder='Digite Seu Nome'
+                            onChange={e => setName(e.target.value)}
+                        /> 
+                        <input 
+                            type='text' 
+                            placeholder='Digite sua cidade'
+                            onChange={e => setCity(e.target.value)}
+                        /> 
+                    </form>
+                    
+                    <button onClick={createUser}> Enviar </button>
+                </div>
+            </main>
+           
         </div>
             
     )
